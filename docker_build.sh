c05f31e4339a766690349a0e3a599aacb0df4fdb
jaked3 <42357602+jaked3@users.noreply.github.com>
2019-11-21 21:08:26 -0500

    z

--- docker_build.sh
+++ docker_build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 echo "script worked"
+if [ -d "SESP-Web-UI" ]; then
+    echo "GIT REPO EXISTS... PULLING"
+    cd SESP-Web-UI
+    git pull
+fi
+elif
+    echo "GIT REPO DOES NOT EXIST... CLONING"
+    git clone https://github.com/CARLETON-SESP/SESP-Web-UI.git
+    cd SESP-Web-UI
+fi
 
-# git clone https://github.com/CARLETON-SESP/SESP-Web-UI.git
-# cd SESP-Web-UI
 # sudo docker image build -t sespsyscon/sesp-web-ui . 
