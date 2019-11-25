b060be2f7a6a27d452a026e438935a05adc364fc
joshgreenwood15 <joshuagreenwood@cmail.carleton.ca>
2019-11-24 20:39:27 -0500

    playing with the promise

--- src/chart-configs/dashboard_fourth_chart.js
+++ src/chart-configs/dashboard_fourth_chart.js
@@ -13,8 +13,8 @@ let url = `http://ec2-34-230-39-178.compute-1.amazonaws.com:8080/`
   });
   
   console.log(promise1);
-  console.log(promise1.PromiseValue.main);
-  console.log(promise1.PromiseValue.main.temp);
+  console.log(promise1.PromiseValue);
+  console.log(promise1.object);
 var fourth_chart_month = {
   chart: {
     showBorder: "0",
